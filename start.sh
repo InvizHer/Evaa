56ba544aeb002c6fbd638b18fc47b87c3ab216ba
InvizHer <112791490+InvizHer@users.noreply.github.com>
2023-12-31 16:56:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/InvizHer/Evaa-3.0.git /TG_BOTZ
+  git clone https://github.com/InvizHer/Evaa.git /TG_BOTZ
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TG_BOTZ
+  git clone $UPSTREAM_REPO /Evaa
 fi
 cd /TG_BOTZ
 pip3 install -U -r requirements.txt
