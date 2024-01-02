5e3fb000e9ec53df820f13b4dfc4aaefcc295509
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-01-02 21:40:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2337,7 +2337,7 @@ async def auto_filter(client, msg, spoll=False):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"Hey {query.from_user.mention} 🙋🏻‍♂️\n\nHere's reasult for {search}"
+            cap = f"Hey {message.from_user.mention} 🙋🏻‍♂️\n\nHere's reasult for {search}"
         else:
             # cap = f"Hey {message.from_user.mention} 🙋🏻‍♂️\n\nHere's reasult for {search}"
             cap = f"<b>Hᴇʏ {message.from_user.mention}, Fᴏᴜɴᴅ {total_results} Rᴇsᴜʟᴛs ғᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}\n\n</b>"
