323d8c8759bc4fe76578b3aaa2ca3cad83e13001
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-01-07 22:35:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,8 +44,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://codexun:TeamCodexun07@codexun.egmx5.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "DotexMovies")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'MoviesV3')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Emixina")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Movies')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
