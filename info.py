b34859f9c8d0c4b1b86aaad47373db32ea35fd3a
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-01-07 22:26:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,13 +15,13 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '10098309'))
 API_HASH = environ.get('API_HASH', 'aaacac243dddc9f0433c89cab8efe323')
-BOT_TOKEN = environ.get("BOT_TOKEN", "5473067352:AAFR4GeX5qauM9KU__DhJUWv68Dct85Xftg")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6699641330:AAHwjck1g2f2ZU0jlCPLoJPUkVFWM16s8Tg")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 99999))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/334f55c6c77100e6b85e3.mp4')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/33472b8bd6fca69fe74d3.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/41a08d01363c18ca909f1.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
