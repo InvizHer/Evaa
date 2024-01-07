bfc66657ebc57ec57ba70efc719955c7187a5f33
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-01-07 22:53:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -96,9 +96,8 @@ async def start(client, message):
                     InlineKeyboardButton('ᴅᴏɴᴀᴛᴇ 💳', callback_data='kushal') 
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)      
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
