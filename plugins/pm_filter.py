df56492d95c084af41cb97e40aaff19d3358a12c
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-01-10 10:39:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1577,13 +1577,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="■ □ □"
+            text="🔐"
         )
         await query.message.edit_text(
-            text="■ ■ □"
-        )
-        await query.message.edit_text(
-            text="■ ■ ■"
+            text="🔐"
         )
         await query.message.edit_text(
             text=script.KUSHAL_TXT,
