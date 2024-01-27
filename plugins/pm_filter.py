2c2c6ac3e6b4d89ddc51dd5d84f4210bd5e5cdc3
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-01-27 13:51:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2242,10 +2242,10 @@ async def auto_filter(client, msg, spoll=False):
             cap = f"<b>{search} - {total_results} reasults 👇</b>"
         else:
             # cap = f"<b>{search} : {total_results} Reasults\n\nOn Request - {message.from_user.mention}</b>"
-            cap = f"<b>{message.from_user.mention} 🙋🏻‍♂️, found {total_results} reasults for {search}.\n\n</b>"
+            cap = f"<b>Hey {message.from_user.mention} 🙋🏻‍♂️ found {total_results} reasults for {search}.\n\n</b>"
             cap+="<b><u>Select and click the name 👇</u></b>\n\n"
             for file in files:
-                cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
+                cap += f"<b><a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>{get_size(file.file_size)} - {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
 
     if imdb and imdb.get('poster'):
         try:
